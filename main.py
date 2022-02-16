eb13f5d642759b6f15270095f10149ab07f08895
Pliszka <karoliniakkjk@gmail.com>
2022-02-16 12:09:07 +0100

    request params

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import os
 import requests
 
-ID = os.environ.get('nutritionix_id')
+ID = os.environ.get('nutritionix_ID')
 KEY = os.environ.get('nutritionix_key')
 
-NUTRI_ENDPOINT = 'https://trackapi.nutritionix.com/v2/search/instant'
+NUTRI_ENDPOINT = 'https://trackapi.nutritionix.com/v2/natural/exercise'
 
-nutri_params = {
-
+nutri_params_exercise = {
+ "query":"ran 3 miles",
+ "gender":"female",
+ "weight_kg":61.5,
+ "height_cm":164,
+ "age":19
 }
 
-response = requests.get(url=NUTRI_ENDPOINT)
+header = {
+    'x-app-id': ID,
+    'x-app-key': KEY
+}
+
+response = requests.post(url=NUTRI_ENDPOINT, json=nutri_params_exercise, headers=header)
+response.raise_for_status()
