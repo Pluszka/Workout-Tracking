f2a5bb396e401e48c78e535ce1c00b12b0313d0c
Pliszka <karoliniakkjk@gmail.com>
2022-02-16 15:37:34 +0100

    workout params google sheet

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import os
 import requests
+import datetime as dt
 
 ID = os.environ.get('nutritionix_ID')
 KEY = os.environ.get('nutritionix_key')
 
+GENDER = "female"
+WEIGHT_KG = 61.5
+HEIGHT_CM = 164
+AGE = 19
+
+SHEETY_ENDPOINT = os.environ.get('SHEETY_WORKOUTS')
 NUTRI_ENDPOINT = 'https://trackapi.nutritionix.com/v2/natural/exercise'
 
 nutri_params_exercise = {
  "query": input('Which exercises have you done?(+amount of time/reps)'),
- "gender": "female",
- "weight_kg": 61.5,
- "height_cm": 164,
- "age": 19
+ "gender": GENDER,
+ "weight_kg": WEIGHT_KG,
+ "height_cm": HEIGHT_CM,
+ "age": AGE
 }
 
 header = {
@@ -22,4 +29,14 @@ header = {
 response = requests.post(url=NUTRI_ENDPOINT, json=nutri_params_exercise, headers=header)
 response.raise_for_status()
 data = response.json()
-print(data)
+
+for exercise in data['exercises']:
+    params_row = {
+        'workouts': {
+            'Date': dt.date.today(),
+            'Time': dt.datetime.now().time(),
+            'Exercise': exercise['user_input'],
+            'Duration': exercise['duration_min'],
+            'Calories' exercise['nf_calories'],
+        }
+    }
